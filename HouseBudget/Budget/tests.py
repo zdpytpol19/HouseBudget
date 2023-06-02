8a99312c43657b91d5e694dd16a8f59a52ca639f
Konrad Skiba <konrad81@o2.pl>
2023-06-02 21:16:33 +0200

    Add templaty.

--- HouseBudget/Budget/tests.py
+++ HouseBudget/Budget/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+class MyFirstTestCase(TestCase):
+    def test_success(self):
+        assert 1 ==1, "jesli warunek nie przejdzie komunikat wyswietli sie wraz z błędem"
+        self.assertTrue(True)
+
+    def test_fail(self):
+        self.assertTrue(False)
+
+    def test_error(self):
+        raise ValueError("Error has been raise")
+
